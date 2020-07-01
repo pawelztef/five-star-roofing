0a4bcc361e0d927f1b3cdb3b9d20598878d919ce
Pavel <pawelztef@gmail.com>
2020-07-01 12:59:46 +0100

    Base styles for footer.

--- five_star_roofing_frontend/src/components/Footer.js
+++ five_star_roofing_frontend/src/components/Footer.js
@@ -3,65 +3,49 @@ import Brand from './Brand'
 
 
 const footer = () => (
-  <div class="pz-container-wrapper">
-    <footer className="pz-footer">
-      <div className="pz-footer__col-1">
-        <address className='pz-contact-details'>
-          <div className="pz-contact-details__address">
-            <h6>Address</h6>
-            <span>
-              8500, Lorem Street,
-            </span>
-            <span>
-              Chicago, IL, 55030
-            </span>
-          </div>
-          <div className="pz-contact-details__email">
-            <h6>Email</h6>
-            <span>
-              <a href="mailto:sales@example.com">sales@example.com</a>
-            </span>
-          </div>
-          <div className="pz-contact-details__phone">
-            <span>
+  <footer className="pz-footer__wrapper">
+    <div className="pz-row">
+      <div className="pz-container pz-footer">
+
+
+        <div className="pz-footer__col-1">
+          <Brand />
+        </div>
+
+        <div className="pz-footer__col-2">
+          <address className='pz-contact-details'>
+            <div>
+              <h6>Address</h6>
+              <span>8500, Lorem Street,</span>
+              <span>Chicago, IL, 55030 </span>
+            </div>
+            <div>
+              <h6>Email</h6>
+              <span><a href="mailto:sales@example.com">sales@example.com</a></span>
+            </div>
+            <div>
               <h6>Phone</h6>
-              <a href="tel:1234567890">(123) 456-78-90</a>
-            </span>
-            <span>
-              <a href="tel:1234567890">(123) 456-78-90</a>
-            </span>
+              <span><a href="tel:1234567890">(123) 456-78-90</a></span>
+              <span><a href="tel:1234567890">(123) 456-78-90</a></span>
+            </div>
+          </address>
+        </div>
+
+        <div className="pz-footer__col-3">
+          <div className="pz-opening-hours">
+            <span><h6>Monday:</h6> 24h</span>
+            <span><h6>Tuesday:</h6> 24h</span>
+            <span><h6>Wednesday:</h6> 24h</span>
+            <span><h6>Thursday:</h6> 24h</span>
+            <span><h6>Friday:</h6> 24h</span>
+            <span><h6>Saturday:</h6> 24h</span>
+            <span><h6>Sunday:</h6> 24h</span>
           </div>
-        </address>
-      </div>
-      <div className="pz-footer__col-2">
-        <Brand />
-        <div className="pz-footer-meta">
-          <span>
-            <a href="https://pawelztef.me">Pawelztef</a>© 2020
-          </span>
-          <span>
-            All rights reserved
-          </span>
         </div>
+
       </div>
-      <div className="pz-footer__col-3">
-        <div className="pz-footer__legal">
-          <span className="pz-footer__legal-item">
-            <a href="http://pawelztef.me">Privacy Policy</a>
-          </span>
-          <span className="pz-footer__legal-item">
-            <a href="http://pawelztef.me">Cookies Policy</a>
-          </span>
-          <span className="pz-footer__legal-item">
-            <a href="http://pawelztef.me">Terms And Conditions</a>
-          </span>
-          <span className="pz-footer__legal-item">
-            <a href="http://pawelztef.me">Refund Policy</a>
-          </span>
-        </div>
-      </div>
-    </footer>
-  </div>
+    </div>
+  </footer>
 )
 
 export default footer
