9ae63280476ea49735df2cfba6edcf3e3e0914a8
Pavel <pawelztef@gmail.com>
2020-07-04 22:15:18 +0100

    Props added to Banner component.

--- five_star_roofing_frontend/src/components/HomePage.js
+++ five_star_roofing_frontend/src/components/HomePage.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import Banner from './Banner'
 
-const homePage = () => {
-  return(
-    <>
-      
-      <Banner />
-    
-    </>
-  )
+
+class HomePage extends React.Component{
+  render(){
+    return(
+      <>
+
+      <Banner 
+        bannerTitle="Five Star Roofing provide best quality service around Dublin"
+        bannerSubtitle="ask for more details"
+        bannerCallToAction="Request a free quote"
+        bannerLink="/contact"
+      />
+
+      </>
+    )
+  }
 }
 
-export default homePage
+export default HomePage
