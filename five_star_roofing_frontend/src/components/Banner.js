1ba48edf960cfe1eaed125bc64a114e33aa6e106
Pavel <pawelztef@gmail.com>
2020-07-04 21:44:58 +0100

    Banner Styled.

--- five_star_roofing_frontend/src/components/Banner.js
+++ five_star_roofing_frontend/src/components/Banner.js
@@ -3,8 +3,12 @@ import React from 'react'
 const Banner = () => {
   return(
     <div className="pz-banner__wrapper">
-      <div className="pz-banner__container">
-        Banner
+      <div className="pz-container pz-banner__container">
+        <h1 className="pz-banner__title">
+          Five Star Roofing provide best quality service around Dublin
+        </h1>
+        <p className="pz-banner__subtitle">ask for more details</p>
+        <button className="pz-banner__button">Request a free quote</button>
       </div>
     </div>
   )
