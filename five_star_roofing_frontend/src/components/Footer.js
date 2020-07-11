975e7a96408eab2212e352bcc9e22415202f6814
Pavel <pawelztef@gmail.com>
2020-07-11 13:58:38 +0100

    Footer fourth column added.

--- five_star_roofing_frontend/src/components/Footer.js
+++ five_star_roofing_frontend/src/components/Footer.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import Brand from './Brand'
 import ContactDetails from './ContactDetails'
+import { MdPhoneInTalk } from "react-icons/md";
 
 
 const footer = () => (
   <footer className="pz-footer__wrapper">
     <div className="pz-row">
-      <div className="pz-container pz-footer">
-
-
+      <div className="pz-footer">
         <div className="pz-footer__col-1">
-          <Brand />
+          <p>
+            If you are looking for a reliable and reputed roofing contractor in
+            Ireland with a proven record delivering high quality workmanship at 
+            affordable prices then you have come to the right place. Five Star Roofing
+            is a leading roofing contractor in Ireland and has the expertise, experience 
+            and professionalism accumulated over 20 years to execute projects undertaken 
+            on time and within budget.
+          </p>
         </div>
 
         <div className="pz-footer__col-2">
@@ -32,15 +38,20 @@ const footer = () => (
             </div>
           </div>
         </div>
-
+        <div className="pz-footer__col-4">
+          <a className="pz-menu__item pz-menu__item-dark" href="tel:00353873539296">
+            <span>< MdPhoneInTalk /></span>
+          <span>Emenrgency Repair</span>
+        </a>
       </div>
     </div>
-    <div className="pz-footer-meta">
-      <span>
-        Designed by <a href="https://pawelztef.me">IrishWebsites.ie</a>
-      </span>
-    </div>
-  </footer>
+  </div>
+  <div className="pz-footer-meta">
+    <span>
+      Designed by <a href="https://pawelztef.me">IrishWebsites.ie</a>
+    </span>
+  </div>
+</footer>
 )
 
 export default footer
