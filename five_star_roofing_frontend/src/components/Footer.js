32bcba9f42ea37c8126e59bb58e3b76d9e128b5d
Pavel <pawelztef@gmail.com>
2020-07-11 14:27:44 +0100

    Footer columns styles.

--- five_star_roofing_frontend/src/components/Footer.js
+++ five_star_roofing_frontend/src/components/Footer.js
@@ -9,28 +9,37 @@ const footer = () => (
     <div className="pz-row">
       <div className="pz-footer">
         <div className="pz-footer__col-1">
-          <p>
-            If you are looking for a reliable and reputed roofing contractor in
-            Ireland with a proven record delivering high quality workmanship at 
-            affordable prices then you have come to the right place. Five Star Roofing
-            is a leading roofing contractor in Ireland and has the expertise, experience 
-            and professionalism accumulated over 20 years to execute projects undertaken 
-            on time and within budget.
-          </p>
-        </div>
-
-        <div className="pz-footer__col-2">
-          <div className="pz-contact-details__wrapper">
-            <ContactDetails />
+          <div className="pz-footer__content">
+            <h5 className="pz-footer__title">About Five Star Roofing</h5>
+            <p>
+              If you are looking for a reliable and reputed roofing contractor in
+              Ireland with a proven record delivering high quality workmanship at 
+              affordable prices then you have come to the right place. Five Star Roofing
+              is a leading roofing contractor in Ireland and has the expertise, experience 
+              and professionalism accumulated over 20 years to execute projects undertaken 
+              on time and within budget.
+            </p>
           </div>
         </div>
 
-        
+        <div className="pz-footer__col-2">
+          <div className="pz-footer__content">
+            <div className="pz-contact-details__wrapper">
+            <h5 className="pz-footer__title">Emenrgency line</h5>
+              <ContactDetails />
+            </div>
+          </div>
+        </div>
+
+
         <div className="pz-footer__col-4">
-          <a className="pz-menu__item pz-menu__item-dark" href="tel:00353873539296">
-            <span>< MdPhoneInTalk /></span>
-          <span>Emenrgency Repair</span>
-        </a>
+          <div className="pz-footer__content">
+            <h5 className="pz-footer__title">Emenrgency line</h5>
+            <a className="pz-menu__item pz-menu__item-dark" href="tel:00353873539296">
+              <span>< MdPhoneInTalk /></span>
+            <span>Emenrgency Repair</span>
+          </a>
+        </div>
       </div>
     </div>
   </div>
